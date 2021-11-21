fbdcc34eccabb64b399579441a9ba06685b71171
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-11-22 01:43:31 +0600

    doctors api created

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 7000;
 // jwt token auth
 const admin = require("firebase-admin");
@@ -15,6 +18,7 @@ admin.initializeApp({
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nu4vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -36,10 +40,11 @@ async function verifyToken(req, res, next) {
 async function run() {
   try {
     await client.connect();
-    console.log("db connected");
+    console.log("doctors portal db connected");
     const database = client.db("doctors_portal");
     const appointmentsCollection = database.collection("appointments");
     const usersCollection = database.collection("users");
+    const doctorsCollection = database.collection("doctors");
 
     // get appointments from server and show to client
     app.get("/appointments", async (req, res) => {
@@ -50,14 +55,63 @@ async function run() {
       const appointmensts = await cursor.toArray();
       res.json(appointmensts);
     });
+
+    // get appointmet by id
+    app.get("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await appointmentsCollection.findOne(query);
+      res.json(result);
+    });
     // post appoints from client sidde to server
-    app.post("/appointments", verifyToken, async (req, res) => {
+    app.post("/appointments", async (req, res) => {
       const appointment = req.body;
 
       const result = await appointmentsCollection.insertOne(appointment);
       console.log(appointment);
       res.json(result);
       // some
+    });
+
+    // update appointment pay info
+    app.put("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      console.log(id, payment);
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          payment: payment,
+        },
+      };
+      const result = await appointmentsCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    app.get("/doctors", async (req, res) => {
+      const cursor = doctorsCollection.find({});
+      const doctors = await cursor.toArray();
+      res.json(doctors);
+    });
+
+    // doctors collection with image
+    app.post("/doctors", async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const pic = req.files.image;
+      const picData = pic.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const doctor = {
+        name,
+        email,
+        image: imageBuffer,
+      };
+      const result = await doctorsCollection.insertOne(doctor);
+
+      console.log("body", req.body);
+      console.log("files", req.files);
+      res.json(result);
     });
 
     // check user is admin or not
@@ -106,6 +160,18 @@ async function run() {
       } else {
         res.status(403).json({ message: "you do not have access now" });
       }
+    });
+    // payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const paymentInfo = req.body;
+      console.log(paymentInfo);
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.json({ clientSecret: paymentIntent.client_secret });
     });
   } finally {
     // await client.close();
